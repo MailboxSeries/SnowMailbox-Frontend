ca492a00a95afad43d5c4eca92ffac3982cc2e7e
minseong0324 <kwan0324@naver.com>
2023-11-21 04:06:19 +0900

    Fix: 폰트 경로 문제 해결

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <RecoilRoot>
         <App />
       </RecoilRoot>
-    </React.StrictMode>,
+    </React.StrictMode>
   </QueryClientProvider>
-  
 )
