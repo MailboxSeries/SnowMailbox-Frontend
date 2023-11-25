58ba499c3847fc5eeac4fef3cbd4903f3213a8d5
minseong0324 <kwan0324@naver.com>
2023-11-25 15:40:24 +0900

    Fix : 미사용 props 이슈 해결

---
+++ src/assets/Picture/index.ts
@@ -0,0 +1,9 @@
+import Picture1 from './1.png';
+import Picture2 from './2.png';
+import Picture3 from './3.png';
+
+export const Picture = [
+    { index: 1, imgSrc: Picture1 },
+    { index: 2, imgSrc: Picture2 },
+    { index: 3, imgSrc: Picture3 },
+];  
