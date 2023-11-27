617162679e8704928f567455d6b84f16dc2b06f3
minseong0324 <kwan0324@naver.com>
2023-11-28 03:00:58 +0900

    Feat : 회원가입(기본) api 로직 추가

---
+++ src/apis/auth.ts
@@ -0,0 +1,19 @@
+import {instance} from './axios';
+
+  // 회원가입(기본)
+  export const postCompletedMissionChecked = async (
+    email: string, 
+    nickname: string, 
+    password: string
+) => {
+    try {
+      await instance.post(
+        `/api/v1/user/sign-up`, {
+            "email" : email,
+            "nickname" : nickname,
+            "password" : password
+        });
+    } catch (error) {
+      return null;
+    }
+  };
