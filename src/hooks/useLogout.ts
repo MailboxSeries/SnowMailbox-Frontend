61ab525abef6f2b5f59c23d849c9de8e8f7f2184
minseong0324 <kwan0324@naver.com>
2023-11-28 07:57:24 +0900

    Feat: 로그아웃 구현

--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -1,21 +1,42 @@
+import { useNavigate } from 'react-router-dom';
+import { postSignOut } from './../apis/auth';
 import { skinDataState } from '@/atoms/SkinAtom';
 import { userInfoAtom } from './../atoms/SignInAtom';
 import { HomeDataAtom } from '@/atoms/HomeAtom';
 import { useResetRecoilState } from 'recoil';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import useIsMyHome from './useIsMyHome';
 
 export default function useLogout() {
   const resetHomeData = useResetRecoilState(HomeDataAtom);
   const resetData = useResetRecoilState(userInfoAtom);
   const resetSkinData = useResetRecoilState(skinDataState);
+  const {ownerId, myId, isMyHome} = useIsMyHome();
 
   const logout = () => {
-    // Recoil 상태를 초기화합니다
-    resetHomeData();
-    resetData();
-    resetSkinData();
+    const navigate = useNavigate();
 
-    // 로그아웃 관련 로직
-    console.log('Logged out');
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation({
+      mutationFn: () => 
+      postSignOut(myId),
+      onError: (error) => {
+        console.error('오류: ', error);
+      },
+      onSuccess: () => {
+        queryClient.invalidateQueries({queryKey: ['userInfo']});
+        // Recoil 상태를 초기화합니다
+        resetHomeData();
+        resetData();
+        resetSkinData();
+        navigate('/');
+      },
+    });
+  
+    const GoOut = () => {
+      mutation.mutate();
+    };
   };
 
   return logout;
