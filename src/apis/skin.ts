dd51e073886d989e71f7ffdd4d4c0d2c1f009b57
minseong0324 <kwan0324@naver.com>
2023-11-27 19:53:38 +0900

    Feat : api 추가

--- src/apis/skin.ts
+++ src/apis/skin.ts
@@ -25,7 +25,7 @@ export const getUnCompletedById = async (myId: string, missionId: string) => {
   };
 
   // 아직 깨지 않은 미션의 완료버튼을 누를 시 
-export const getCompletedMissionById = async (myId: string, missionId: string, missionChecked: boolean) => {
+export const postCompletedMissionChecked = async (myId: string, missionId: string, missionChecked: boolean) => {
     try {
       await instance.post(
         `/api/v1/mission/${missionId}/${myId}`, {
@@ -35,3 +35,41 @@ export const getCompletedMissionById = async (myId: string, missionId: string, m
       return null;
     }
   };
+
+    // 마지막 미션은 “크리스마스” 입력하는 건데, 이거에 대한 요청 따로 해야해서 만든 api
+export const postCompletedChristmas = async (myId: string, missionId: string, missionAnswer: string) => {
+    try {
+        const response = await instance.post(
+        `/api/v1/mission/${missionId}/${myId}/christmas`, {
+            "missionAnswer" : missionAnswer 
+        });
+        return response.data //missionAnswerIsChristmas
+    } catch (error) {
+        return null;
+    }
+};
+
+    // 스킨을 모두 선택하고 난 후, 스킨 적용 버튼을 눌렀을 때
+    export const postSelectedSkin = async (
+        myId: string, 
+        treeType: number,
+        characterType: number,
+        starType: number,
+        boxType: number,
+        ornamentType: number
+        
+        ) => {
+        try {
+            await instance.post(
+            `/api/v1/user/${myId}/skin-apply`, {
+                "treeType" : treeType,
+                "characterType" : characterType,
+                "starType" : starType,
+                "boxType" : boxType,
+                "ornamentType" : ornamentType
+            });
+
+        } catch (error) {
+            return null;
+        }
+    };
