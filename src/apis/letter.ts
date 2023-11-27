a310cc202233b0c0801f1c87fad6bab4249da3b3
minseong0324 <kwan0324@naver.com>
2023-11-28 02:35:40 +0900

    Feat :  편지 확인 모달에서 각각의 해당하는 날짜의 버튼을 눌렀을 때 api 호출 로직 추가

---
+++ src/apis/letter.ts
@@ -0,0 +1,14 @@
+import {instance} from './axios';
+
+//  편지 확인 모달에서 각각의 해당하는 날짜의 버튼을 눌렀을 때
+export const getDayLetter = async (day: number, myId: string) => {
+    try {
+      const response = await instance.get(
+        `/api/v1/letter/${day}/${myId}`,
+      );
+      return response.data; //treeList, ornamentList, starList, boxList
+    } catch (error) {
+      return null;
+    }
+  };
+  
