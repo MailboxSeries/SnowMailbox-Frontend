d6fc1415ad6fa5bfb3e1be3cf9093cb056e75a4f
minseong0324 <kwan0324@naver.com>
2023-11-28 07:47:35 +0900

    Feat: 리다이렉트 구현

--- src/pages/Redirect/Redirect.tsx
+++ src/pages/Redirect/Redirect.tsx
@@ -1,7 +1,26 @@
+import { getMyIdAtRedirectPage } from '@/apis/auth';
+import { Data, initialUserInfoState, userInfoAtom } from '@/atoms/SignInAtom';
 import PageLayout from '@/components/PageLayout/PageLayout'
+import { useSuspenseQuery } from '@tanstack/react-query';
 import React from 'react'
+import { Navigate, useNavigate } from 'react-router';
+import { useSetRecoilState } from 'recoil';
 
 export default function Redirect() {
+    const setUserInfoState = useSetRecoilState(userInfoAtom);
+    const navigate = useNavigate();
+    const {data} = useSuspenseQuery<Data>({
+        queryKey: ['userInfo'],
+        queryFn: () => getMyIdAtRedirectPage(),
+      });
+
+      if (data !== null) {
+        setUserInfoState(data);
+        navigate(`/home/${data.myId}`)
+      } else {
+        setUserInfoState(initialUserInfoState);
+      }
+      
   return (
     <PageLayout>로그인 중...</PageLayout>
   )
