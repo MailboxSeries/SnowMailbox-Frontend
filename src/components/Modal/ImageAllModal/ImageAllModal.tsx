52958b07ade594ec046804ac7740ae76101a4157
minseong0324 <kwan0324@naver.com>
2023-11-28 17:01:09 +0900

    Fix : 타입 에러 해결

--- src/components/Modal/ImageAllModal/ImageAllModal.tsx
+++ src/components/Modal/ImageAllModal/ImageAllModal.tsx
@@ -13,11 +13,6 @@ type Props = {
 
 function ImageAllModal({closeModal, isOpen, selectedDate}: Props) {
     const {ownerId, myId, isMyHome} = useIsMyHome();
-    const [imageList, setImageList] = useState([
-        "https://example.com/image1.jpg",
-        "https://example.com/image2.jpg",
-        "https://example.com/image3.jpg"
-    ]); // 이미지 리스트를 위한 상태
 
     const {data} = useQuery({
         queryKey: ['images', myId],
@@ -25,28 +20,30 @@ function ImageAllModal({closeModal, isOpen, selectedDate}: Props) {
         staleTime: 10000,
     });
 
-    if (data !== null) {
-        setImageList(data.imageList);
-    } else {
-        alert("데이터를 가져오는 데에 실패했어요. 다시 로그인 해주세요.")
-        //TODO: 로그인 페이지로 
+      // data가 아직 로드되지 않은 경우, null을 반환
+    if (!data) {
+        return null;
     }
+
+    // data가 로드된 후에 이미지 목록을 설정
+    const imageList = data.imageList || [];
 
     return (
         <Modal
-        isOpen={isOpen}
-        onClose={closeModal}
-        modalTitle={`12월 ${selectedDate}일`}
-        imageType={'Modal'}
+            isOpen={isOpen}
+            onClose={closeModal}
+            modalTitle={`12월 ${selectedDate}일`}
+            imageType={'Modal'}
         >
-        <S.Wrapper>
-            {imageList.map((imageSrc, index) => (
-            <S.LetterContent key={index}>
-                <S.LetterImage src={imageSrc} />
-            </S.LetterContent>
-            ))}
-        </S.Wrapper>
+            <S.Wrapper>
+                {data !== null && imageList.map((imageSrc, index) => (
+                <S.LetterContent key={index}>
+                    <S.LetterImage src={imageSrc} />
+                </S.LetterContent>
+                ))}
+            </S.Wrapper>
         </Modal>
     );
+    
 }
 export default React.memo(ImageAllModal);
