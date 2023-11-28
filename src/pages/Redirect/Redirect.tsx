67305edaac9d56b41261ed8743d0dc0879197145
minseong0324 <kwan0324@naver.com>
2023-11-29 00:12:12 +0900

    Fix : 리다이렉트 해결

--- src/pages/Redirect/Redirect.tsx
+++ src/pages/Redirect/Redirect.tsx
@@ -2,26 +2,34 @@ import { getMyIdAtRedirectPage } from '@/apis/auth';
 import { Data, initialUserInfoState, userInfoAtom } from '@/atoms/SignInAtom';
 import PageLayout from '@/components/PageLayout/PageLayout'
 import { useSuspenseQuery } from '@tanstack/react-query';
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Navigate, useNavigate } from 'react-router';
 import { useSetRecoilState } from 'recoil';
 
 export default function Redirect() {
     const setUserInfoState = useSetRecoilState(userInfoAtom);
     const navigate = useNavigate();
-    const {data} = useSuspenseQuery<Data>({
-        queryKey: ['userInfo'],
-        queryFn: () => getMyIdAtRedirectPage(),
-        staleTime: 10000,
-      });
 
-      if (data !== null) {
-        setUserInfoState(data);
-        navigate(`/home/${data.myId}`)
-      } else {
-        setUserInfoState(initialUserInfoState);
-      }
-      
+    const GoToHome = async () => {
+
+      const {data} = useSuspenseQuery<Data>({
+          queryKey: ['userInfo'],
+          queryFn: () => getMyIdAtRedirectPage(),
+          staleTime: 10000,
+        });
+
+        if (data !== null) {
+          setUserInfoState(data);
+          navigate(`/home/${data.myId}`)
+        } else {
+          setUserInfoState(initialUserInfoState);
+        }
+    }
+
+    useEffect(() => {
+      GoToHome();
+    }, []);
+    
   return (
     <PageLayout>로그인 중...</PageLayout>
   )
