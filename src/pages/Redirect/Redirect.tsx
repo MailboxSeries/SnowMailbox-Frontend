53cdd91180925d8351faa32f6fe5c1483a522331
minseong0324 <kwan0324@naver.com>
2023-11-29 00:21:53 +0900

    Fix : 리다이렉트 해결

--- src/pages/Redirect/Redirect.tsx
+++ src/pages/Redirect/Redirect.tsx
@@ -9,27 +9,25 @@ import { useSetRecoilState } from 'recoil';
 export default function Redirect() {
     const setUserInfoState = useSetRecoilState(userInfoAtom);
     const navigate = useNavigate();
-
-    const GoToHome = async () => {
-
-      const {data} = useSuspenseQuery<Data>({
-          queryKey: ['userInfo'],
-          queryFn: () => getMyIdAtRedirectPage(),
-          staleTime: 10000,
-        });
-
-        if (data !== null) {
-          setUserInfoState(data);
-          navigate(`/home/${data.myId}`)
-        } else {
-          setUserInfoState(initialUserInfoState);
-        }
-    }
-
     useEffect(() => {
+      const GoToHome = async () => {
+        const {data} = useSuspenseQuery<Data>({
+            queryKey: ['userInfo'],
+            queryFn: () => getMyIdAtRedirectPage(),
+            staleTime: 10000,
+            gcTime: 15000
+          });
+
+          if (data !== null) {
+            setUserInfoState(data);
+            navigate(`/home/${data.myId}`)
+          } else {
+            setUserInfoState(initialUserInfoState);
+          }
+      }
       GoToHome();
     }, []);
-    
+
   return (
     <PageLayout>로그인 중...</PageLayout>
   )
