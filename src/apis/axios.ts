4b27bf2c6d4f8be80850e34c8ded570a217385d6
minseong0324 <kwan0324@naver.com>
2023-11-29 00:57:35 +0900

    Fix : 리다이렉트 이슈

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -5,29 +5,29 @@ import useSetTokens from '../hooks/useSetTokens';
 export const instance = axios.create({
   baseURL: 'https://snowmailbox.com',
   headers: {
-    Authorization: `Bearer ${Cookies.get('accessToken')}`,
+    Authorization: `${localStorage.getItem('accessToken')}`,
   },
 });
 
 instance.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    async (error) => {
-        const originalRequest = error.config;
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
 
-        if (error.response.code === 401 && !originalRequest._retry) {
-        originalRequest._retry = true;
-        const refreshToken = Cookies.get('refreshToken');
-        if (!refreshToken) {
-            throw new Error('토큰 없음');
-        }
-        try {
-            await sendRefreshToken(refreshToken);
-            return instance(originalRequest);
-        } catch (error) {
-            throw error;
-        }
+    if (error.response.code === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const refreshToken = localStorage.getItem('refreshToken');
+      if (!refreshToken) {
+        throw new Error('토큰 없음');
+      }
+      try {
+        await sendRefreshToken(refreshToken);
+        return instance(originalRequest);
+      } catch (error) {
+        throw error;
+      }
     }
 
     return Promise.reject(error);
@@ -38,7 +38,7 @@ const sendRefreshToken = async (refreshToken) => {
   try {
     const response = await instance.get('/auth/token', {
       headers: {
-        refreshToken: `Bearer ${refreshToken}`,
+        refreshToken: `${refreshToken}`,
       },
     });
 
