de05e1d615044b121c32043dc61d05cf1bbeea6f
minseong0324 <kwan0324@naver.com>
2023-11-30 15:08:36 +0900

    Fix : atom default값 수정

--- src/atoms/HomeAtom.ts
+++ src/atoms/HomeAtom.ts
@@ -4,13 +4,13 @@ import {atom} from 'recoil';
 export const HomeDataAtom = atom<HomeData>({
   key: 'Data',
   default: {
-    nickname : "겨울이",
+    nickname : "   ",
 	treeType : 1, // 디폴트는 1
 	characterType : 1, // 디폴트는 1
 	starType : 0, //없을 경우 0
 	boxType : 0,  //없을 경우 0
 	ornamentType : 1, //디폴트는 1
-	nowDate : 23, // 가입한 날부터 1일
+	nowDate : 1, // 가입한 날부터 1일
   },
 });
 
