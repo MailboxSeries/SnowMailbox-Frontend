9763193f999d455c64769d58d7485a0a2ffd8ee1
minseong0324 <kwan0324@naver.com>
2023-11-30 20:58:22 +0900

    Fix: 요청 문제

--- src/components/Modal/Letter/LetterReadModal/LetterReadModal.tsx
+++ src/components/Modal/Letter/LetterReadModal/LetterReadModal.tsx
@@ -24,27 +24,32 @@ type Letter = {
 function LetterReadModal({closeModal, isOpen, selectedDate}: Props) {
     const {ownerId, myId, isMyHome} = useIsMyHome();
     const navigate = useNavigate();
-    const [letters, setLetters] = useState<Letter[]>([ //TODO: 더미데이터. 배포시 삭제
-        {
-            sender:"ㅇㅇ",
-            image:"",
-            content:"ㅇㅇ"
-        }
-    ]); // 선택된 날짜의 편지들을 저장할 상태입니다.
+    const [letters, setLetters] = useState<Letter[]>([]); // 선택된 날짜의 편지들을 저장할 상태입니다.
 
     
-    const {data} = useSuspenseQuery({
-        queryKey: ['dayLetter', myId],
-        queryFn: () => getDayLetter(selectedDate, myId),
-        staleTime: 0,
-        gcTime: 0
-    });
+    // const {data} = useSuspenseQuery({
+    //     queryKey: ['dayLetter', myId],
+    //     queryFn: () => getDayLetter(selectedDate, myId),
+    //     staleTime: 0,
+    //     gcTime: 0,
+    // });
 
     useEffect(() => {
-      if (data) {
-        setLetters(data.letterList);
+      const fetchData = async () => {
+
+          try {
+              const response = await getDayLetter(selectedDate, myId);
+              setLetters(response.letterList); // 요청 성공 시 데이터 설정
+          } catch (err) {
+            alert('세션이 만료되었어요. 다시 로그인 해주세요.')
+            navigate('/signin')
+          } 
+      };
+
+      if (isOpen) {
+          fetchData();
       }
-  }, [data]);
+  }, [isOpen, selectedDate, myId]); // 의존성 배열에 isOpen, selectedDate, myId 추가
 
     return (
         <Modal
@@ -55,7 +60,7 @@ function LetterReadModal({closeModal, isOpen, selectedDate}: Props) {
         >
         <S.Wrapper>
           <S.InnerWrapper>
-            {data !== null && letters.length > 0 ? (
+            {letters !== null && letters.length > 0 ? (
               letters.map((letter, index) => (
                 <S.LetterContent key={index}>
                     <S.TextsStyle>
