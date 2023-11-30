4d12399faa03f198141ed1dbe8d0322865b8444f
minseong0324 <kwan0324@naver.com>
2023-11-30 18:18:07 +0900

    Fix : 스크롤 복구

--- src/components/Modal/SkinModal/SkinModal.tsx
+++ src/components/Modal/SkinModal/SkinModal.tsx
@@ -169,7 +169,7 @@ function SkinModal({closeModal, isOpen}: Props) {
           <Carousel 
           showThumbs={false} 
           showStatus={false} 
-          centerMode={false} // 중앙정렬 해제
+          centerMode
           centerSlidePercentage={33}>
           {Tree.map((tree, index) => (
                 <S.SelectClickEvent 
