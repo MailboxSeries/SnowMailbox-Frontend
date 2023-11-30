941b01740582febb2a0c6137bd8969a718210644
minseong0324 <kwan0324@naver.com>
2023-11-30 20:59:46 +0900

    Fix: 요청 문제

--- src/components/Modal/ImageAllModal/ImageAllModal.tsx
+++ src/components/Modal/ImageAllModal/ImageAllModal.tsx
@@ -16,20 +16,38 @@ function ImageAllModal({closeModal, isOpen, selectedDate}: Props) {
     const {ownerId, myId, isMyHome} = useIsMyHome();
     const navigate = useNavigate();
     const [imageList, setImageList] = useState([]);
-    const {data} = useQuery({
-        queryKey: ['images', myId],
-        queryFn: () => getDayImages(selectedDate, myId),
-        staleTime: 0,
-        gcTime: 0
-    });
+    // const {data} = useQuery({
+    //     queryKey: ['images', myId],
+    //     queryFn: () => getDayImages(selectedDate, myId),
+    //     staleTime: 0,
+    //     gcTime: 0
+    // });
 
+
+    // useEffect(() => {
+    // // data가 로드된 후에 이미지 목록을 설정
+    //     if (data) {
+    //         setImageList(data.imageList);
+    //     }
+    // }, [data]);
 
     useEffect(() => {
-    // data가 로드된 후에 이미지 목록을 설정
-        if (data) {
-            setImageList(data.imageList);
+        const fetchData = async () => {
+  
+            try {
+                const response = await getDayImages(selectedDate, myId);
+                setImageList(response.imageList); // 요청 성공 시 데이터 설정
+            } catch (err) {
+              alert('세션이 만료되었어요. 다시 로그인 해주세요.')
+              navigate('/signin')
+            } 
+        };
+  
+        if (isOpen) {
+            fetchData();
         }
-    }, [data]);
+    }, [isOpen, selectedDate, myId]); // 의존성 배열에 isOpen, selectedDate, myId 추가
+  
 
 
     return (
@@ -41,7 +59,7 @@ function ImageAllModal({closeModal, isOpen, selectedDate}: Props) {
         >
             <S.Wrapper>
                 <S.InnerWrapper>
-                    {data !== null && imageList.length > 0 ? (
+                    {imageList !== null && imageList.length > 0 ? (
                         imageList.map((imageSrc, index) => (
                             <S.LetterContent key={index}>
                                 <S.LetterImage src={imageSrc} />
