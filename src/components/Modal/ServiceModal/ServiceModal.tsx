b62ad1b220f8b7692d9fe01b98f1f36b789c5825
minseong0324 <kwan0324@naver.com>
2023-11-30 17:28:35 +0900

    Feat : 이용안내 모달 내용

--- src/components/Modal/ServiceModal/ServiceModal.tsx
+++ src/components/Modal/ServiceModal/ServiceModal.tsx
@@ -10,7 +10,9 @@ type Props = {
 
 function ServiceModal({closeModal, isOpen}: Props) {
 
-
+  const handleNavigateInstagram = () => {
+    window.location.href = 'https://instagram.com/snowmailbox_official?igshid=NzZlODBkYWE4Ng%3D%3D&utm_source=qr';
+}
  //TODO: 내용 집어넣어야 함
   return (
     <Modal
@@ -31,11 +33,53 @@ function ServiceModal({closeModal, isOpen}: Props) {
           </S.SubTitle>
           <S.TextWrapper>
             <S.DiscriptionText>
-              눈꽃우편함 이용방법<br/>
-              
+              {"눈꽃우편함 이용방법 \n"}
             </S.DiscriptionText>
+            <S.Text>
+            {"-내 트리를 만들고 지인들에게 링크를 공유해요."}
+            </S.Text>
+            <S.Text>
+            {"-미션을 수행하며 스킨을 획득해 트리를 더 멋지게 꾸며요."}
+            </S.Text>
+            <S.Text>
+            {"-지인에게 편지와 추억이 담긴 사진을 받을 수 있어요."}
+            </S.Text>
+            <S.Text>
+            {"-오늘 편지를 받지 못하더라도 걱정말아요. 눈꽃나라 친구들이 따뜻한 위로의 편지를 보내줄거니까요."}
+            </S.Text>
+
+            <S.DiscriptionText>
+              {"눈꽃우편함 인스타그램 공식계정\n"}
+            </S.DiscriptionText>
+            <S.InstagramButton onClick={handleNavigateInstagram}>
+              @snowmailbox_official
+            </S.InstagramButton>
+            <S.Text>
+            {"-인스타그램 공식계정에서 공지사항을 확인해주세요!"}
+            </S.Text>
+            <S.Text>
+            {"-불편사항/건의사항 등이 있다면 인스타그램 공식계정을 통해 문의 부탁드려요."}
+            </S.Text>
+
+            <S.DiscriptionText>
+              {"우편함 Series\n"}
+            </S.DiscriptionText>
+            <S.Text>
+            {"-단풍 우편함에 이어 겨울 시즌을 위한 서비스에요."}
+            </S.Text>
+            <S.Text>
+            {"-겨울에 이어 봄, 여름에도 출시 예정이니 많은 기대 부탁드려요!"}
+            </S.Text>
+            <S.Text>
+            {"-눈꽃 우편함은 동국대, 삼육대 학생 4명에서 만든 크리스마스 시즌 서비스예요."}
+            </S.Text>
+            <S.Text>
+            {"-눈꽃 우편함은 수익을 창출하지 않는 비영리 서비스예요."}
+            </S.Text>
           </S.TextWrapper>
           
+          
+
       </S.Wrapper>
     </Modal>
   );
--- src/components/Modal/ServiceModal/style.ts
+++ src/components/Modal/ServiceModal/style.ts
@@ -40,16 +40,57 @@ export const SubTitle = styled.span`
 export const DiscriptionText = styled.span`
   color: ${theme.colors.textNavy};
   text-align: center;
-  line-height: 1;
+  line-height: 1.8;
   font-size: 14px;
     font-style: normal;
   font-weight: 700;
+  white-space: pre-line;
+  //padding-top: 10px;
 `;
 
 
 export const TextWrapper = styled.div`
   width: 230px;
   height: 600px;
-  margin-top: 15px;
+  margin-top: 25px;
+  flex-direction: column;
 
+`;
+
+export const Text = styled.span`
+    margin-top: 0px;
+    margin-left: 5px;
+    font-family: 'BareunHipi';
+    font-size: 13px;
+    padding-right: 0px;
+    padding-left: 0px;
+    margin-left: 7px;
+    margin-top: -4px;
+    margin-bottom: 2px;
+    white-space: pre-line;
+    line-height: 1.5;
+    display: flex;
+
+`;
+
+export const InstagramButton = styled.button`
+  background:  #7c7e9a;
+  background-size: 180px 36px;
+  z-index: 2;
+  width:180px;
+  height:36px;
+  margin-left: 5px;
+  margin-top: 2px;
+  margin-bottom: 5px;
+  padding-left: 17px;
+  padding-top: 8px;
+  align-items:center;
+  border: transparent;
+  border-radius: 10px;
+  font-size: 16px;
+  color : ${theme.colors.textNavy};
+  &:active {
+    background-color: rgba(128, 128, 128, 0.5);  // 반투명한 회색
+    border-radius: 10px;
+  }
 `;
