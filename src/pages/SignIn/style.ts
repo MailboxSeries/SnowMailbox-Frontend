cf883d2849f87fd3b18ece223f756558fc1afd86
minseong0324 <kwan0324@naver.com>
2023-12-01 19:38:27 +0900

    Fix : 로그인 페이지 마진

--- src/pages/SignIn/style.ts
+++ src/pages/SignIn/style.ts
@@ -18,6 +18,7 @@ export const Container = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    margin-top : 180px;
 `;
 
 export const ButtonText = styled.span`
